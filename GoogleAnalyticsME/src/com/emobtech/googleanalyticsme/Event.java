18dbacb265ad6c21508f5922e7629696ba3a1a74
eMob Tech <admin@emobtech.com>
2010-12-22 17:21:15 +0000

    Javadoc.

--- GoogleAnalyticsME/src/com/emobtech/googleanalyticsme/Event.java
+++ GoogleAnalyticsME/src/com/emobtech/googleanalyticsme/Event.java
@@ -1,12 +1,60 @@
+/*
+ * Event.java
+ * 21/12/2010
+ * Google Analytics ME
+ * Copyright(c) Ernandes Mourao Junior (ernandes@gmail.com)
+ * All rights reserved
+ */
 package com.emobtech.googleanalyticsme;
 
 import com.emobtech.googleanalyticsme.util.StringUtil;
 
+/**
+ * <p>
+ * This class represents an Event that can be used to record user interaction
+ * with application's elements. For instance, you can track how many times the
+ * users hit the Play or Stop button.
+ * </p>
+ * <p>
+ * For further information on Event tracking, access
+ * <a href="http://code.google.com/apis/analytics/docs/tracking/eventTrackerOverview.html" target="_blank">
+ *     Event Tracking Overview
+ * </a>.
+ * </p>
+ * 
+ * @author Ernandes Mourao Junior (ernandes@gmail.com)
+ * @since 1.0
+ * @see Tracker
+ */
 public final class Event implements Request {
-	
+	/**
+	 * <p>
+	 * Event request value.
+	 * </p>
+	 */
 	private String event;
 	
-	public Event(String category, String action, String label, String value) {
+	/**
+	 * <p>
+	 * Creates an instance of Event class.
+	 * </p>
+	 * <p>
+	 * For further information on event's values, access
+	 * <a href="http://code.google.com/apis/analytics/docs/tracking/eventTrackerGuide.html" target="_blank">
+	 *     Event Tracking Guide
+	 * </a>.
+	 * @param category The name you supply for the group of objects you want to
+	 *                 track.
+	 * @param action   A string that is uniquely paired with each category, and 
+	 *                 commonly used to define the type of user interaction for 
+	 *                 the web object. 
+	 * @param label    An optional string to provide additional dimensions to 
+	 *                 the event data. 
+	 * @param value    An integer that you can use to provide numerical data
+	 *                 about the user event. 
+	 * @throws If category or action is empty.
+	 */
+	public Event(String category, String action, String label, Integer value) {
 		if (StringUtil.isEmpty(category)) {
 			throw new IllegalArgumentException("Category must not be empty.");
 		}
@@ -25,15 +73,18 @@ public final class Event implements Request {
 			event.append(action);
 		}
 		event.append(')');
-		if (!StringUtil.isEmpty(value)) {
+		if (value != null) {
 			event.append('(');
-			event.append(action);
+			event.append(value.intValue());
 			event.append(')');
 		}
 		//
 		this.event = event.toString();
 	}
 
+	/**
+	 * @see com.emobtech.googleanalyticsme.Request#url(java.lang.String)
+	 */
 	public String url(String trackingCode) {
 		TrackingURL params = new TrackingURL(trackingCode);
 		params.addParameter("utmt", "event");
--- GoogleAnalyticsME/src/com/emobtech/googleanalyticsme/Tracker.java
+++ GoogleAnalyticsME/src/com/emobtech/googleanalyticsme/Tracker.java
@@ -1,3 +1,10 @@
+/*
+ * Tracker.java
+ * 21/12/2010
+ * Google Analytics ME
+ * Copyright(c) Ernandes Mourao Junior (ernandes@gmail.com)
+ * All rights reserved
+ */
 package com.emobtech.googleanalyticsme;
 
 import java.io.IOException;
@@ -11,24 +18,94 @@ import com.emobtech.googleanalyticsme.util.StringUtil;
 import javax.microedition.io.Connector;
 import javax.microedition.io.HttpConnection;
 
+/**
+ * <p>
+ * This class is responsible for tracking an application's events, in order to
+ * send to Google Analytics. A Tracker object must be associated to given Google
+ * Analytics tracking code, which represents a application's account.
+ * </p>
+ * 
+ * @author Ernandes Mourao Junior (ernandes@gmail.com)
+ * @since 1.0
+ */
 public final class Tracker {
-	
+	/**
+	 * <p>
+	 * Pool of Tracker objects. Only one object per tracking code.
+	 * </p>
+	 */
 	private static Hashtable trackerPool;
 	
+	/**
+	 * <p>
+	 * Google Analytics tracking code.
+	 * </p>
+	 */
 	private String trackingCode;
 	
+	/**
+	 * <p>
+	 * Queue of events.
+	 * </p>
+	 */
 	private Vector queue;
 	
+	/**
+	 * <p>
+	 * Timer used by the background sending process.
+	 * </p>
+	 */
 	private Timer timer;
 	
+	/**
+	 * <p>
+	 * Task used by the timer.
+	 * </p>
+	 */
 	private Task task;
 	
+	/**
+	 * <p>
+	 * Automatic events flushing interval.
+	 * </p>
+	 */
 	private long flushInterval;
 	
+	/**
+	 * <p>
+	 * Creates a Tracker object associated to the given tracking code.
+	 * </p>
+	 * <p>
+	 * A Tracker object instantiated from this method does not flush events 
+	 * automatically. Actually, all queued events must be flushed explicitly by
+	 * calling {@link Tracker#flush(boolean)}.
+	 * </p>
+	 * @param trackingCode App's tracking code.
+	 * @return Tracker object.
+	 * @throws IllegalArgumentException If tracking code is empty.
+	 */
 	public synchronized static Tracker getInstance(String trackingCode) {
 		return getInstance(trackingCode, 0);
 	}
 	
+	/**
+	 * <p>
+	 * Creates a Tracker object associated to the given tracking code.
+	 * </p>
+	 * <p>
+	 * A Tracker object instantiated from this method flushes automatically all
+	 * queued events. The automatic flush is a fixed-delay execution process,
+	 * which delay is according to the given flush interval parameter.
+	 * </p>
+	 * <p>
+	 * In addition, explicit calls to {@link Tracker#flush(boolean)} will flush
+	 * the events, without affecting the automatic process.
+	 * </p>
+	 * @param trackingCode App's tracking code.
+	 * @param flushInterval Automatic flush interval (in seconds).
+	 * @return Tracker object.
+	 * @throws IllegalArgumentException If tracking code is empty.
+	 */
 	public synchronized static Tracker getInstance(String trackingCode,
 		long flushInterval) {
 		if (StringUtil.isEmpty(trackingCode)) {
@@ -50,6 +127,13 @@ public final class Tracker {
 		return tracker;
 	}
 	
+	/**
+	 * <p>
+	 * Creates an instance of Tracker class.
+	 * </p>
+	 * @param trackingCode App's tracking code.
+	 * @param flushInterval Automatic flush interval (in seconds).
+	 */
 	private Tracker(String trackingCode, long flushInterval) {
 		flushInterval *= 1000; //secs to millis.
 		//
@@ -65,6 +149,20 @@ public final class Tracker {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Tracks a given application's event. This methods immediately sends the
+	 * request to Google Analytics.
+	 * </p>
+	 * <p>
+	 * Since it is a synchronous request, this method performs a blocking
+	 * operation. For asynchronous requests, use
+	 * {@link Tracker#addToQueue(Request)} method.
+	 * </p>
+	 * @param request Event.
+	 * @see PageView
+	 * @see Event
+	 */
 	public void track(Request request) {
 		try {
 			process(request);
@@ -73,16 +171,42 @@ public final class Tracker {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Tracks a given application's event. This methods does not immediately
+	 * sends the request to Google Analytics. Actually, it is added to a queue,
+	 * in order to be sent afterwards, by a background process.
+	 * </p>
+	 * <p>
+	 * To force that all queued requests be sent right away, just call the
+	 * {@link Tracker#flush(boolean)} method.
+	 * </p>
+	 * @param request Event.
+	 * @see PageView
+	 * @see Event
+	 */
 	public void addToQueue(Request request) {
 		synchronized (queue) {
 			queue.addElement(request);
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Returns the events queue's size.
+	 * </p>
+	 * @return Size.
+	 */
 	public int queueSize() {
 		return queue.size();
 	}
 	
+	/**
+	 * <p>
+	 * Forces all queued events to be sent to Google Analytics.
+	 * </p>
+	 * @param asynchronously Send event asynchronously (non-blocking).
+	 */
 	public void flush(boolean asynchronously) {
 		if (queue.size() > 0) {
 			if (asynchronously) {
@@ -93,6 +217,11 @@ public final class Tracker {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Processes all queued events.
+	 * </p>
+	 */
 	synchronized void processQueue() {
 		int size = queue.size();
 		int head = 0;
@@ -110,6 +239,13 @@ public final class Tracker {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Process a given requests, in order to send the event to Google Analytics.
+	 * </p>
+	 * @param request Request.
+	 * @throws IOException If any I/O error accours.
+	 */
 	synchronized void process(Request request) throws IOException {
 		String url = request.url(trackingCode);
 		String userAgent = "Google Analytics ME/1.0 (compatible; Profile/MIDP-2.0 Configuration/CLDC-1.0)";
@@ -131,6 +267,12 @@ public final class Tracker {
 		}
 	}
 	
+	/**
+	 * <p>
+	 * Runs the flush process after a given delay.
+	 * </p>
+	 * @param delay Delay (in millis).
+	 */
 	void runTask(long delay) {
 		if (!task.isRunning) {
 			if (flushInterval > 0) {
@@ -151,12 +293,32 @@ public final class Tracker {
 		}
 	}
 
+	/**
+	 * <p>
+	 * Task that executes the flushing process.
+	 * </p>
+	 * 
+	 * @author Ernandes Mourao Junior (ernandes@gmail.com)
+	 * @since 1.0
+	 */
 	private class Task extends TimerTask {
-		
+		/**
+		 * <p>
+		 * End of last execution time.
+		 * </p>
+		 */
 		public long endExecutionTime = System.currentTimeMillis();
 		
+		/**
+		 * <p>
+		 * If the task is running.
+		 * </p>
+		 */
 		public boolean isRunning;
 		
+		/**
+		 * @see java.util.TimerTask#run()
+		 */
 		public void run() {
 			isRunning = true;
 			//
